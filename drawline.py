565699b075cbcf96ef10d27000a772277b398d57
god <emailtesting973@gmail.com>
2024-03-08 11:01:24 +0530

    first commit

---
+++ drawline.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+img=cv2.imread(r"php4.jpg",1)
+image=cv2.resize(img,(300,300))
+
+start_point = (50, 50)
+end_point = (250, 250)
+
+color = (255, 0, 0)  
+
+thickness = 4
+
+cv2.line(image, start_point, end_point, color, thickness)
+
+cv2.imshow('Line Drawing', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
